7c70844ec1308b5e54c61499feffe5b19e945f39
William4184 <williamvonschrottky@yahoo.com>
2023-04-21 12:11:47 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const { readFile } = require('fs');
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// [START gae_node_request_example]
 const express = require('express');
+
 const app = express();
-app.get('/', (request, response) => {
 
-    readFile('./testHTML.html', 'utf8', (err, html) => { 
-    
-        if (err)
-        { 
-            response.status(500).send('sorry, out of order'); 
-        }
-
-        response.send(html);
-    
-    });
+app.get('/', (req, res) => {
+  res.status(200).send('This is my GPT app!').end();
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('app available on http://localhost:3000'));
+// Start the server
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
